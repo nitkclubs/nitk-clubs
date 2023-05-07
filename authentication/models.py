b9c763ca6605030eeb98a8823ef8875504122163
rustammahilane <rustammahilane@gmail.com>
2023-05-07 21:19:29 +0530

    student model error corrected

--- authentication/models.py
+++ authentication/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Student(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models
     rollno = models.IntegerField(null=False, default=0)
     semester = models.IntegerField()
     department = models.CharField(max_length=5)
     # joinedClub = models.ForeignKey()
-
-    def __str__(self):
-        return self.name
